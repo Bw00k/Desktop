4f84931acbeae5a100f78373411ed522b194e127
bw00k <bw00k@naver.com>
2022-12-28 15:17:38 +0900

    28일

---
+++ sdasa/node/node-project/route/customer.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+const route = Router();
+
+route
+
+  .get("/", (req, res) => {
+    res.send("get");
+  })
+
+  .post("/", (req, res) => {
+    res.send("post");
+  })
+  .put("/", (req, res) => {
+    res.send("put");
+  })
+  .delete("/", (req, res) => {
+    res.send("delete");
+  });
+
+export default route;
