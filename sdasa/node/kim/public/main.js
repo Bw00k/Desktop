9f2be17971ea7b9ca4e146b8f4073e16f55fdeaf
bw00k <bw00k@naver.com>
2023-01-03 17:51:07 +0900

    프로젝트2일차

---
+++ sdasa/node/kim/public/main.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const FileStore = require("session-file-store")(session);
+
+const app = express();
+const port = 5500;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "~~~", // 원하는 문자 입력
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore(),
+  })
+);
