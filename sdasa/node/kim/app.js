fd35839e314a255aa1d0a3b6f260c77b9625087b
bw00k <bw00k@naver.com>
2023-01-04 17:44:59 +0900

    수요일3

--- sdasa/node/kim/app.js
+++ sdasa/node/kim/app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var shopRouter = require("./routes/shop");
 var orderRouter = require("./routes/order");
+var ctmidRouter = require("./routes/ctmid");
 
 var app = express();
 
--- sdasa/node/kim/routes/ctmid.js
+++ sdasa/node/kim/routes/ctmid.js
@@ -2,15 +2,4 @@ const pool = require("./pool");
 var express = require("express");
 var router = express.Router();
 
-addbtn.addEventListener("click", function (ev) {
-  let addObj = {
-    no: no.value,
-    pname: pname.value,
-    prodno: prodno.value,
-    address: address.value,
-    phone: phone.value,
-  };
-  ctmid.push(addObj);
-});
-
 module.exports = router;
